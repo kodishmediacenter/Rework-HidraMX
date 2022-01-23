d0135b5fddbef315a041ddbd90053e9b3b5b209a
kodishmediacenter <fabiolmglgeo@gmail.com>
2022-01-23 11:52:18 -0300

    Create richard.php

---
+++ richard.php
@@ -0,0 +1,1 @@
+[["Richard Rasmussen","channel\/UC13ikrGSy3E2AveqLAI9lqg","https://yt3.ggpht.com\/ytc\/AKedOLQV9VTV2drPG9C0OU3-o4hCbaif0m9pQ45nFEsVwg=s256-c-k-c0x00ffffff-no-rj"],["A casa dos Bichos","channel\/UC2c4diB2934SIT78dTdNSiA","https://yt3.ggpht.com\/ytc\/AKedOLTkpjOkJjmdd2NVC3Xy5cD4I8ySl3ghMpY1aNnL=s256-c-k-c0x00ffffff-no-rj"]]
